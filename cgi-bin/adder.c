80da12cb1b72ae4f6ac02dc1e23c625b228151dd
李若飞 <38347214+Ruofei-Li@users.noreply.github.com>
2019-08-16 11:29:56 +0800

    Create adder.c

---
+++ cgi-bin/adder.c
@@ -0,0 +1,31 @@
+#include "helper.h"
+
+int main(void) {
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1=0, n2=0;
+
+    //提取两个参数
+    if ((buf = getenv("QUERY_STRING")) != NULL) {
+	p = strchr(buf, '&');
+	*p = '\0';
+	strcpy(arg1, buf);
+	strcpy(arg2, p+1);
+	n1 = atoi(arg1);
+	n2 = atoi(arg2);
+    }
+
+    //生成响应体
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", 
+	    content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks for visiting!\r\n", content);
+  
+    //打印HTTP响应
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+    exit(0);
+}
